da51f46e86000b75a494bb3e1199485c98dd6fb2
Aniket Kumar Sahu <aniket1149sahu@gmail.com>
2026-01-28 00:41:14 +0530

    Add additional pattern printing scripts for character arrangements

--- Pattern_Questions.py
+++ Pattern_Questions.py
@@ -42,3 +42,34 @@ for i in range(1, num2+1):
     for j in range(1, i + 1):
         print(chr((i*(i-1))//2 + j + 64), end=" ")
     print()
+
+"""
+WAP to print the following pattern
+
+A A A A A
+B B B B
+C C C
+D D
+E
+"""
+num3=int((input("Enter the number of rows: ")))
+for i in range(1, num3+1):
+    for j in range(num3 - i + 1):
+        print(chr(i + 64), end=" ")
+    print()
+    
+"""
+WAP to print the following pattern
+
+A B C D E
+B C D E
+C D E
+D E
+E
+"""
+num4=int((input("Enter the number of rows: ")))
+for i in range(1, num4+1):
+    for j in range(i, num4 + 1):
+        print(chr(j + 64), end=" ")
+    print()
+    
