377f3f7dff929325c607a1d65d1728582a22c273
Aniket Kumar Sahu <aniket1149sahu@gmail.com>
2026-01-29 03:39:04 +0530

    Add multiplication, square, and cube calculation scripts

---
+++ Multiplication_table.py
@@ -0,0 +1,20 @@
+# WAP to create the multiplication table of a given number
+
+num = int(input("Enter a number: "))  # User input
+for i in range(1, 11):
+    print(f"{num} x {i} = {num * i}")
+    
+    
+# WAP to print square of n natural numbers
+
+num = int(input("Enter a number: "))  # User input
+for i in range(1, num+1):
+    print(f"{i} square {i**2}")
+    
+# WAP to print cube of n natural numbers
+
+num = int(input("Enter a number: "))  # User input
+for i in range(1, num+1):
+    print(f"{i} cube {i**3}")
+    
+
